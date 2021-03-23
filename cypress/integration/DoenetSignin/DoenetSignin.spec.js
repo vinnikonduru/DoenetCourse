03d429369586adb01c79028071b0b118c7c3af99
Vinitha Konduru <vinithak@umn.edu>
2021-03-23 11:51:37 -0500

    no auto signin

---
+++ cypress/integration/DoenetSignin/DoenetSignin.spec.js
@@ -0,0 +1,7 @@
+// email enter in signin
+// click button to send pswd
+// get cookie device name
+// new php request -- using emailid & device name
+// return signin code
+// enter password  and click signin
+
