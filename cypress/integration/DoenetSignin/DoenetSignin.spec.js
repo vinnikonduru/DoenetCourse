b6f30d74d5d97ba34235e5f9e8074ea49be10534
Vinitha Konduru <vinithak@umn.edu>
2021-03-24 15:12:45 -0500

    start signin

--- cypress/integration/DoenetSignin/DoenetSignin.spec.js
+++ cypress/integration/DoenetSignin/DoenetSignin.spec.js
@@ -1,7 +1,14 @@
-// email enter in signin
-// click button to send pswd
+// enter email on /signin to get signin code
+// click button to get signin code
 // get cookie device name
+
 // new php request -- using emailid & device name
 // return signin code
 // enter password  and click signin
+//goes to dashboard
 
+describe('Sign in', () => {
+  it('signin successfully', () => {
+    cy.visit('/signin')
+  })
+})
