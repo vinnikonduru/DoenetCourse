45ddec10d85d8ad3e74682a474b0be5557d1a4c0
Vinitha Konduru <vinithak@umn.edu>
2021-07-13 11:49:51 -0500

    imports fix

--- src/Tools/_framework/Menus/SelectedCourse.jsx
+++ src/Tools/_framework/Menus/SelectedCourse.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import { useRecoilValue,useSetRecoilState,useRecoilStateLoadable } from 'recoil';
-import { fetchDrivesSelector ,fetchDriveUsers} from '../../../_reactComponents/Drive/Drive';
+import { fetchDrivesSelector ,fetchDriveUsers} from '../../../_reactComponents/Drive/NewDrive';
 import { 
   faChalkboard,
   faCode,
